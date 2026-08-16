1ef35614a53943f07645dda529a1480e74a119d3
Alternative Vladislav <kolodaznyjvladislav@gmail.com>
2026-08-16 23:06:03 +0300

    Detach updater from caller input

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Feint VPN Node in-place updater.
 set -Eeuo pipefail
+exec </dev/null
 
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 BLUE='\033[0;34m'; CYAN='\033[0;36m'; BOLD='\033[1m'; NC='\033[0m'
